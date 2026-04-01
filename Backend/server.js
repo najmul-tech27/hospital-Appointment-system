1979d4434534fe99d53908c76e9ce09876422a28
Najmul Khan <Najmul5245khan@gmail.com>
2026-04-01 23:48:11 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,8 @@ cloudinary.v2.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server listening on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
